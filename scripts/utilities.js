7ffaf27ec281a2422acb5853157494f6ac4b6e96
Chad Rider <crider17@gmail.com>
2017-10-01 15:56:17 -0400

    Added updated utilities.js file to this checkpoint

---
+++ scripts/utilities.js
@@ -0,0 +1,20 @@
+//alert("Leaving here for testing purposes");
+
+var pointsArray = document.getElementsByClassName('point');
+
+var numOfNodes = pointsArray.length;
+
+var forEach = function(numOfNodes, callback) {
+
+  for (index = 0; index < numOfNodes; index++) {
+    callback(index);
+  }
+
+}
+
+//Call back funtion
+function alertAll(point) {
+  alert("Selling point #" + point + " is a great selling point!");
+}
+
+forEach(numOfNodes, alertAll);
